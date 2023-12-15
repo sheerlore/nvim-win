ab4947db3609a17d4d5f6cbbbf8fc410874af0c0
uai <noema01@outlook.jp>
2023-12-15 17:14:35 +0900

    add lualine

--- init.lua
+++ init.lua
@@ -1,3 +1,10 @@
 -- init.lua
 
 require('000-options')
+
+-- plugins
+require('plugins')
+require('101-lualine')
+
+
+vim.cmd [[colorscheme tokyonight-night]]
---
+++ lua/plugins.lua
@@ -0,0 +1,14 @@
+vim.cmd [[packadd packer.nvim]]
+
+return require("packer").startup(function(use)
+  use "wbthomason/packer.nvim"
+  -- color theme
+  use "folke/tokyonight.nvim"
+  -- icon
+  use "nvim-tree/nvim-web-devicons"
+  -- status line
+  use {
+    "nvim-lualine/lualine.nvim",
+    requires = { "nvim-tree/nvim-web-devicons", opt = true }
+  }
+end)
